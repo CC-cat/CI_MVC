997b0a124d37f6b078d20c5361ad096bd709a386
CC-cat <2752053902@qq.com>
2018-07-05 00:20:37 +0800

    no message

---
+++ CodeIgniter_test1/application/views/templates/header.php
@@ -0,0 +1,11 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+<!DOCTYPE html>
+<html lang="zh-CN" dir="ltr">
+    <head>
+        <title>CodeIgniter Tutorial</title>
+    </head>
+    <body>
+
+        <h1><?php echo $title; ?></h1>
