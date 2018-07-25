4a3c0a243f31e9e1ff68615094bbf1bb8c440546
CC-cat <2752053902@qq.com>
2018-07-25 09:13:52 +0800

    no message

--- CodeIgniter_test1/application/config/routes.php
+++ CodeIgniter_test1/application/config/routes.php
@@ -1,5 +1,5 @@
 <?php
-defined('BASEPATH') or exit('No direct script access allowed');
+defined('BASEPATH') OR exit('No direct script access allowed');
 
 /*
 | -------------------------------------------------------------------------
@@ -49,8 +49,6 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | Examples:	my-controller/index	-> my_controller/index
 |		my-controller/my-method	-> my_controller/my_method
 */
-
-// example.com/index.php/
 $route['default_controller'] = 'welcome';
 $route['404_override'] = '';
-$route['translate_uri_dashes'] = false;
+$route['translate_uri_dashes'] = FALSE;
